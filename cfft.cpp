067a644c5c00c8b56953ca5bc10d71519df76f34
Hesse <35089600+Artorias123@users.noreply.github.com>
2019-03-07 10:33:17 +0800

    Add files via upload

--- cfft.cpp
+++ cfft.cpp
@@ -152,8 +152,8 @@ struct cfft2
     }
     void operator()(complex<double> *out){
         int tt=n,td=1,i,j;
-        complex<T> t1,t2;
-        //T r1,r2,i1,i2,rw,iw;
+        //complex<T> t1,t2;
+        T r1,r2,i1,i2,rw,iw;
         BitReverse(out,n);
         do{
             i=0;
@@ -161,19 +161,20 @@ struct cfft2
             do{
                 j=0;
                 do{
-                    
+                    /*
                     t1=out[i];
                     t2=out[i+td]*W[j*tt];
                     out[i]=t1+t2;
                     out[i+td]=t1-t2;
-                    
-                   /*
+                    */
+                   
                    r1=out[i].real();i1=out[i].imag();
-                   r2=out[i+td].real();i2=out[i+td].imag();
-                   rw=W[j*tt].real();iw=W[j*tt].imag();
+                   r2=out[i+td].real()*W[j*tt].real()-out[i+td].imag()*W[j*tt].imag();
+                   i2=out[i+td].imag()*W[j*tt].real()+out[i+td].real()*W[j*tt].imag();
+                   //rw=W[j*tt].real();iw=W[j*tt].imag();
                    out[i].real(r1+r2);out[i].imag(i1+i2);
                    out[i+td].real(r1-r2);out[i+td].imag(i1-i2);
-                    */
+                    
                     i++;j++;
                 }while(j<td);
                 i+=td;
@@ -196,17 +197,32 @@ struct cfft4
     }
     complex<T> *W;
     cfft4(){
-        W=new complex<T>[n]; 
-        for(int i=0;i<n;i++) {
-            W[i]=complex<T>(cos(2*i*pi()/n),-sin(2*i*pi()/n));
-        }
+        W=new complex<T>[(n>>2)*3];
+        for(int i=0;i<(n>>2)*3;i++) W[i]=complex<T>(cos(2*i*pi()/n),-sin(2*i*pi()/n));
     }
+    /*
+    cfft4(){
+        W=new complex<T>[n-1];
+        int k(4),i(3),t(n>>2);
+        W[0]=W[1]=W[2]=1;
+        do{
+            t>>=2;
+            for(int j=0;j<k;j++){
+                W[i++]=complex<T>(cos(2*j*t*pi()/n),-sin(2*j*t*pi()/n));
+                W[i++]=W[i-1]*W[i-1];
+                W[i++]=W[i-1]*W[i-2];
+            }
+            k<<=2;
+        }while(i<n-1);
+    }
+    */
     ~cfft4(){
         delete []W;
     }
     void operator()(complex<T> *in,complex<T> *out){
         int tt=n,td=1,i,j;
-        complex<T> t1,t2,t3,t4;
+        //complex<T> t1,t2,t3,t4;
+        T r1,r2,r3,r4,i1,i2,i3,i4,rt,it;
         for(i=0;i<n;i++) out[i]=in[i];
         br(out);
         do{
@@ -214,19 +230,31 @@ struct cfft4
             tt>>=2;
             do{
                 j=0;
-                do{
-                    t1=out[i];
-                    t2=out[i+td]*W[j*tt];
-                    t3=out[i+td*2]*W[j*tt*2];
-                    t4=out[i+td*3]*W[j*tt*3];
-                    out[i]=t1+t2+t3+t4;
-                    out[i+td]=t1-muli(t2)-t3+muli(t4);
-                    out[i+td*2]=t1-t2+t3-t4;
-                    out[i+td*3]=t1+muli(t2)-t3-muli(t4);
-                    i++;j++;
-                }while(j<td);
+                r1=out[i].real();i1=out[i].imag();
+                r2=out[i+td].real();i2=out[i+td].imag();
+                r3=out[i+2*td].real();i3=out[i+2*td].imag();
+                r4=out[i+3*td].real();i4=out[i+3*td].imag();
+                out[i].real(r1+r2+r3+r4);out[i].imag(i1+i2+i3+i4);
+                out[i+td].real(r1+i2-r3-i4);out[i+td].imag(i1-r2-i3+r4);
+                out[i+td*2].real(r1-r2+r3-r4);out[i+td*2].imag(i1-i2+i3-i4);
+                out[i+td*3].real(r1-i2-r3+i4);out[i+td*3].imag(i1+r2-i3-r4);
+                i++;j++;
+                for(;j<td;j++){
+                    r1=out[i].real();i1=out[i].imag();
+                    r2=out[i+td].real()*W[j*tt].real()-out[i+td].imag()*W[j*tt].imag();
+                    i2=out[i+td].real()*W[j*tt].imag()+out[i+td].imag()*W[j*tt].real();
+                    r3 = out[i + td * 2].real() * W[j * tt * 2].real() - out[i + td * 2].imag() * W[j * tt * 2].imag();
+                    i3=out[i+td*2].real()*W[j*tt*2].imag()+out[i+td*2].imag()*W[j*tt*2].real();
+                    r4=out[i+td*3].real()*W[j*tt*3].real()-out[i+td*3].imag()*W[j*tt*3].imag();
+                    i4=out[i+td*3].real()*W[j*tt*3].imag()+out[i+td*3].imag()*W[j*tt*3].real();
+                    out[i].real(r1+r2+r3+r4);out[i].imag(i1+i2+i3+i4);
+                    out[i+td].real(r1+i2-r3-i4);out[i+td].imag(i1-r2-i3+r4);
+                    out[i+td*2].real(r1-r2+r3-r4);out[i+td*2].imag(i1-i2+i3-i4);
+                    out[i+td*3].real(r1-i2-r3+i4);out[i+td*3].imag(i1+r2-i3-r4);
+                    i++;
+                }
                 i+=td*3;
-            }while(i<n);
+            } while (i < n);
             td<<=2;
         }while(tt!=1);
     }
@@ -256,19 +284,56 @@ struct cfft4
             td<<=2;
         }while(tt!=1);
     }
+    /*
+    void fftt(complex<T> *in,complex<T> *out){
+        int tt=n,td=1,i,j;
+        T r1,r2,r3,r4,i1,i2,i3,i4,rt,it;
+        complex<double>* w=W;
+        for(i=0;i<n;i++) out[i]=in[i];
+        br(out);
+        do{
+            i=0;
+            tt>>=2;
+            do{
+                j=0;
+                do{
+                    r1=out[i].real();i1=out[i].imag();
+                    r2=out[i+td].real()*w->real()-out[i+td].imag()*w->imag();
+                    i2=out[i+td].real()*w->imag()+out[i+td].imag()*(w++)->real();
+                    r3=out[i+td*2].real()*w->real()-out[i+td*2].imag()*w->imag();
+                    i3=out[i+td*2].real()*w->imag()+out[i+td*2].imag()*(w++)->real();
+                    r4=out[i+td*3].real()*w->real()-out[i+td*3].imag()*w->imag();
+                    i4=out[i+td*3].real()*w->imag()+out[i+td*3].imag()*(w++)->real();
+                   
+                    out[i].real(r1+r2+r3+r4);out[i].imag(i1+i2+i3+i4);
+                    out[i+td].real(r1+i2-r3-i4);out[i+td].imag(i1-r2-i3+r4);
+                    out[i+td*2].real(r1-r2+r3-r4);out[i+td*2].imag(i1-i2+i3-i4);
+                    out[i+td*3].real(r1-i2-r3+i4);out[i+td*3].imag(i1+r2-i3-r4);
+                    i++;j++;
+                }while(j<td);
+                w-=3*td;
+                i+=3*td;
+            }while(i<n);
+            w+=3*td;
+            td<<=2;
+        }while(tt!=1);
+    }
+    */
 };
-#define N 4
+
+#define N 1024
 int main(){
     srand(clock());
     complex<double> x[N],y[N],z[N];
     cfft2<N,double> fft1;
     cfft4<N,double> fft2;
     for(int i=0;i<N;i++) x[i]=rand()%10;
+    //fft2.fftt(x,y);
     //prt(x,N);
     //for(int i=0;i<N;i++) cfft<N,double>(x,y);
     //for(int i=0;i<100*N;i++) fft1(x,y);
-    for(int i=0;i<4;i++) 
-    fft2(x);
+    //for(int i=0;i<100*N;i++) fft1(x);
+    for(int i=0;i<100*N;i++) fft2(x,y);
     //cdft<N,double>(x,z);
     //fft2(x);
     //fft2(x,y);
@@ -277,5 +342,5 @@ int main(){
     //cdft<N,double>(x,z);
     //fft1(x,z);
     //prt(z,N);
-    //dotmul(x,z,N);
+    //dotmul(y,z,N);
 }
